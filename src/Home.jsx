e97abbc6dc075e8e54d02793d5af3d788278c9fb
Andrew <awolfe76@gmail.com>
2018-05-18 13:16:01 -0400

    updates home page content to include for-filers from cf.gov; updates links to filing app to open new window

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,21 +6,35 @@ const Home = () => {
       <div className="usa-grid">
         <div className="usa-width-one-whole">
           <header>
-            <h1>The HMDA Platform</h1>
+            <h1>The Home Mortgage Disclosure Act</h1>
             <p className="usa-font-lead">
-              All things HMDA. From tools to help you prepare your file to
-              access to register, login, and being the filing process, and
-              finally HMDA data publication this is the place to be.
+              HMDA requires many financial institutions to maintain, report, and
+              publicly disclose information about mortgages.
             </p>
           </header>
+
+          <div className="usa-alert usa-alert-info">
+            <div className="usa-alert-body">
+              <h3 className="usa-alert-heading">Announcement</h3>
+              <p className="usa-alert-text">
+                The{' '}
+                <a href="/data-publication/aggregate-reports/2017">
+                  MSA/MD Aggregate Reports
+                </a>{' '}
+                are now available.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="card-container">
         <div className="card">
           <header>
-            <h4>
-              <a href="/filing/">Get started filing your HMDA data</a>
-            </h4>
+            <h3>
+              <a href="/filing/" rel="noopener noreferrer" target="_blank">
+                Get started filing your HMDA data
+              </a>
+            </h3>
             <p>
               Beginning with HMDA data collected in or after 2017, financial
               institutions will use the HMDA Platform to upload their
@@ -29,13 +43,65 @@ const Home = () => {
               filing year.
             </p>
           </header>
+
+          <h4>Help for Filers</h4>
+          <p>
+            Published resources for financial institutions required to file Home
+            Mortgage Disclosure Act (HMDA) data.
+          </p>
+          <ul>
+            <li>Filing Instruction Guide</li>
+            <ul>
+              <li>
+                <a
+                  href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2017-hmda-fig.pdf"
+                  download={true}
+                >
+                  For data collected in 2017
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2018-hmda-fig.pdf"
+                  download={true}
+                >
+                  For data collected in or after 2018
+                </a>
+              </li>
+              <li>
+                For data collected in or before 2016, please visit the{' '}
+                <a href="https://www.ffiec.gov/hmda/fileformats.htm">
+                  FFIEC website
+                </a>{' '}
+                for data submission resources.
+              </li>
+            </ul>
+            <li>
+              <a href="https://www.ffiec.gov/hmda/guide.htm">
+                Getting It Right Guide
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/HMDA-Loan-Scenarios.pdf"
+                download={true}
+              >
+                Loan Scenarios
+              </a>
+            </li>
+            <li>
+              <a href="https://hmdahelp.consumerfinance.gov/knowledgebase/s/">
+                Self Service Knowledge Portal
+              </a>
+            </li>
+          </ul>
         </div>
 
         <div className="card">
           <header>
-            <h4>
+            <h3>
               <a href="/data-publication/">Data Publication</a>
-            </h4>
+            </h3>
             <p>
               The HMDA data and reports are the most comprehensive publicly
               available information on mortgage market activity.
@@ -44,27 +110,33 @@ const Home = () => {
               <li>
                 <a href="/data-publication/modified-lar">Modifed LAR</a>
               </li>
-            </ul>
-            <span
-              className="usa-label"
-              style={{ marginTop: '1.5em', display: 'inline-block' }}
-            >
-              Coming Soon
-            </span>
-            <ul>
-              <li>Disclosure Reports</li>
-              <li>MSA/MD Aggregate Reports</li>
-              <li>National Aggregate Reports</li>
-              <li>Snapshot National Loan-Level Dataset</li>
+              <li>
+                Disclosure Reports{' '}
+                <span className="usa-label" style={{}}>
+                  Coming Soon
+                </span>
+              </li>
+              <li>
+                <a href="/data-publication/aggregate-reports/2017">
+                  MSA/MD Aggregate Reports
+                </a>
+              </li>
+              <li>
+                National Aggregate Reports{' '}
+                <span className="usa-label">Coming Soon</span>
+              </li>
+              <li>
+                <a href="/data-publication/snapshot-national-loan-level-dataset">
+                  Snapshot National Loan-Level Dataset
+                </a>
+              </li>
             </ul>
           </header>
-        </div>
 
-        <div className="card">
           <header>
-            <h4>
+            <h3>
               <a href="/tools/">Tools</a>
-            </h4>
+            </h3>
             <p>
               Here you can find various tools to assist you in getting your HMDA
               LAR ready for filing.
@@ -86,59 +158,6 @@ const Home = () => {
               </li>
             </ul>
           </header>
-        </div>
-      </div>
-
-      <div className="usa-grid">
-        <div className="usa-width-one-whole">
-          <div className="max-width">
-            <h3>CFPB Notice and Consent Banner</h3>
-            <p className="usa-text-small">
-              This is a Consumer Financial Protection Bureau (CFPB) information
-              system. The CFPB is an independent agency of the United States
-              Government. CFPB information systems are provided for the
-              processing of official information only. Unauthorized or improper
-              use of this system may result in administrative action, as well as
-              civil and criminal penalties. Because this is a CFPB information
-              system, you have no reasonable expectation of privacy regarding
-              any communication or data transiting or stored on this information
-              system. All data contained on CFPB information systems is owned by
-              CFPB and your use of the CFPB information system serves as your
-              consent to your usage being monitored, intercepted, recorded,
-              read, copied, captured or otherwise audited in any manner, by
-              authorized personnel, including but not limited to employees,
-              contractors, and/or agents of the United States Government.
-            </p>
-
-            <hr />
-
-            <h3>Paperwork Reduction Act</h3>
-            <p className="usa-text-small">
-              According to the Paperwork Reduction Act of 1995, an agency may
-              not conduct or sponsor, and, not withstanding any other provision
-              of law, a person is not required to respond to a collection of
-              information unless it displays a valid OMB control number. The OMB
-              control number for this collection is 3170-0008. The time required
-              to complete this information collection is estimated to average
-              between 7,700 hours and 77 hours per response depending on the
-              size of the institution, per response. The obligation to respond
-              to this collection of information is mandatory per the Home
-              Mortgage Disclosure Act 12 U.S.C. 2801-2810 as implemented by
-              CFPB’s Regulation C 12 CFR part 1003. Comments regarding this
-              collection of information, including the estimated response time,
-              suggestions for improving the usefulness of the information, or
-              suggestions for reducing the burden to respond to this collection
-              should be submitted to the Bureau at the Consumer Financial
-              Protection Bureau (Attention: PRA Office), 1700 G Street NW,
-              Washington, DC 20552, or by email to PRA@cfpb.gov. The other
-              agencies collecting information under this regulation maintain OMB
-              Control numbers for their collections as follows: Office of the
-              Comptroller of the Currency (1557–0159), the Federal Deposit
-              Insurance Corporation (3064–0046), the Federal Reserve System
-              (7100–0247), the Department of Housing and Urban Development (HUD)
-              (2502–0529), the National Credit Union Administration (3133–0166).
-            </p>
-          </div>
         </div>
       </div>
     </main>
